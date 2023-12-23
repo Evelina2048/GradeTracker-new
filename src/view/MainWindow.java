e964d9afce5f91220a7df79639801e09663490ae
Evelina2048 <evelinaguenther@gmail.com>
2023-12-23 02:31:32 -0500

    layout is correct

--- src/view/MainWindow.java
+++ src/view/MainWindow.java
@@ -1,10 +1,17 @@
 package src.view;
 
 import javax.swing.ButtonGroup;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JRadioButton;
+import javax.swing.border.Border;
+
 import java.awt.FlowLayout;
+//import javax.swing.BoxLayout;
+import java.awt.BorderLayout;
 import java.awt.Font;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
 
 
 public class MainWindow {
@@ -12,6 +19,12 @@ private JFrame window;
 JRadioButton teacherButton;
 JRadioButton studentButton;
 ButtonGroup teacherStudentGroup;
+JPanel instructionsPanel;
+JPanel choicesPanel;
+JLabel instructionsWords;
+JButton backButton;
+JButton nextButton;
+JPanel BackNextbuttons;
 
 
 
@@ -21,11 +34,23 @@ public MainWindow() {
     window = new JFrame();
     window.setTitle("Launcher");
     window.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-    window.setLayout(new FlowLayout());
+    //window.setLayout(new FlowLayout());
+    window.setLayout(new BorderLayout());
     window.setSize(800, 500);
     window.setLocationRelativeTo(null);
 
+    //instructions (north section for borderlayout)
+    instructionsWords = new JLabel("Welcome! Are you a student or a teacher?");
+    instructionsPanel= new JPanel();
+    instructionsPanel.add(instructionsWords);
+    window.add(instructionsPanel, BorderLayout.NORTH);
+
+    //set the font for instructions
+    Font instructionsFont = new Font("Arial", Font.PLAIN, 40); // Change the font and size here
+    instructionsWords.setFont(instructionsFont);
+
     //teacher and student radiobuttons initialize
+    choicesPanel= new JPanel();
     teacherButton = new JRadioButton("Teacher");
     studentButton = new JRadioButton("Student");
     teacherStudentGroup = new ButtonGroup();
@@ -39,8 +64,11 @@ public MainWindow() {
     //teacher and student radiobuttons add to group and make visible
     teacherStudentGroup.add(teacherButton);
     teacherStudentGroup.add(studentButton);
-    window.add(teacherButton);
-    window.add(studentButton);
+    choicesPanel.add(teacherButton);
+    choicesPanel.add(studentButton);
+    window.add(choicesPanel);
+
+    
 
 }
 
