ec50664b04dadcfd417863f20ebd8d3bc4c5cc3f
Evelina2048 <evelinaguenther@gmail.com>
2023-12-22 21:44:44 -0500

    teacher and student button show up

--- src/view/MainWindow.java
+++ src/view/MainWindow.java
@@ -1,10 +1,17 @@
 package src.view;
 
+import javax.swing.ButtonGroup;
 import javax.swing.JFrame;
+import javax.swing.JRadioButton;
+import java.awt.FlowLayout;
+
 
 public class MainWindow {
 //initialization
 private JFrame window;
+JRadioButton teacherButton;
+JRadioButton studentButton;
+
 
 
 
@@ -12,6 +19,19 @@ public MainWindow() {
     window = new JFrame();
     window.setTitle("Launcher");
     window.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    window.setLayout(new FlowLayout());
+
+    teacherButton = new JRadioButton("Teacher");
+    studentButton = new JRadioButton("Student");
+
+    ButtonGroup teacherStudentGroup = new ButtonGroup();
+    teacherStudentGroup.add(teacherButton);
+    teacherStudentGroup.add(studentButton);
+
+    window.add(teacherButton);
+    window.add(studentButton);
+
+
     window.setSize(800, 500);
     window.setLocationRelativeTo(null);
 
