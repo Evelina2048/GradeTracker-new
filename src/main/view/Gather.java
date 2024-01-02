aa08001d329c4f0175e8d28bff2f86e5537f3075
Evelina2048 <evelinaguenther@gmail.com>
2024-01-02 09:35:19 -0500

    most of the junit error is gone

--- src/main/view/Gather.java
+++ src/main/view/Gather.java
@@ -40,7 +40,11 @@ import java.io.FileWriter;
 import java.io.IOException;
 import com.opencsv.CSVWriter;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
 
 
 
