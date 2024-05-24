09116681a08ad3933a6fbda795d68b31166efd7e
Evelina2048 <evelinaguenther@gmail.com>
2024-05-24 02:06:46 -0400

    clean

--- src/main/view/student/StudentStatCollect.java
+++ src/main/view/student/StudentStatCollect.java
@@ -8,32 +8,19 @@ import java.awt.Color;
 import java.awt.Dimension;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-import javax.swing.border.Border;
 import javax.swing.JLabel;
 
 import java.awt.event.ActionListener;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.awt.event.ActionEvent;
 import java.awt.FlowLayout;
 import java.awt.Component;
-import java.io.BufferedReader;
 
 import java.util.ArrayList;
 import main.view.NewUser;
 import main.view.Creator;
 import main.view.Set;
 
-import javax.swing.JPanel;
-import javax.swing.JTextField;
 import java.awt.Container;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.awt.Component;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -47,15 +34,12 @@ public class StudentStatCollect extends JFrame {
     private JPanel container = new JPanel(new FlowLayout(FlowLayout.LEFT));
     private JPanel classLabelPanelContainer = new JPanel(new BorderLayout());
     private JPanel newDelContainerFlow;
-    //private JPanel classLabelPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
     private JPanel classLabelPanel = new JPanel(new FlowLayout(FlowLayout.LEFT, 5, 5));
-    //private int index = set.getClassListIndex();
     private int typeNumber = 0;
     private int numOfBoxes = 0;
 
     private int maxBoxes = 25;
     
-    //private boolean underMaxBoxes = (numOfBoxes <=27);
     private JPanel northCreditsPanel;
     private JPanel northNewTypePanel;
     private JPanel allBoxesPanel = new JPanel();
@@ -64,25 +48,18 @@ public class StudentStatCollect extends JFrame {
     private final ArrayList<String> finalClassList;
 
     public StudentStatCollect(JFrame main,NewUser newUser, String studentOrTeacher, String existingOrNew, Set set) {
-        //set.resetContainerAndPanel();
-        System.out.println("classlistinstudentstatcollectbegining"+ set.getClassList());
         this.window = main;
         this.set = set;
-        //private final ArrayList<String> finalClassList = set.getFinalClassList();
         this.finalClassList = new ArrayList<>(set.getFinalClassList());
-        System.out.println("finalclasslistafterinitialization: : "+finalClassList);
         creator = new Creator(set);
         studentStatCollectLaunch(window);
         createNewTypeButton();
         buttonSetUpAction(main, newUser, studentOrTeacher, existingOrNew);
-        //
         DisplayClasses();
-        //
     }
 
     public void studentStatCollectLaunch(JFrame window) {
         classList = set.getClassList();
-        System.out.println("in student classes...........********"+classList);
         window.setTitle("StudentStatCollect");
         window.setPreferredSize(new Dimension(1000, 1000));
     }
@@ -106,26 +83,13 @@ public class StudentStatCollect extends JFrame {
         saveButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 //check if textbox is vaild
-
-                //if valid. regex?
                 Pattern pattern = Pattern.compile("^(?:[0-9]*(?:.[0-9]+))*\s*$|^[0-9]*\s*$", Pattern.CASE_INSENSITIVE);
 
                 correctGradeFormatChecker(pattern);
 
-                //Matcher matcher = pattern.matcher("88");
-                //should check match 
-                //boolean matchFound = matcher.find();
-                //System.out.println("matchfound? "+matchFound);
-                //
                 saveButton.setEnabled(false);
                 creator.setTextFieldPanel(classLabelPanel);
-                System.out.println("classlabelpanel save print::::::: "+classLabelPanel.getComponentCount()+" "+numOfBoxes);
-                System.out.println("array test!!!!!!!!: "+classLabelPanel.getComponentCount()+" "+numOfBoxes);
-                //creator.writeTextToFile("/Users/evy/Documents/GradeTracker-new/src/main/view/UserInfo/StudentInfo/" + set.getUsername() +"/"+classList.get(set.getClassListIndex())+ ".txt", creator.getTextFieldContainer());
-                System.out.println("primary bug testser pppppp:::: "+ finalClassList);//gives bug
-                System.out.println("primary bug testser:::: "+ finalClassList.get(set.getClassListIndex()));//gives bug
                 creator.writeTextToFile("/Users/evy/Documents/GradeTracker-new/src/main/view/UserInfo/StudentInfo/" + set.getUsername() +"/"+finalClassList.get(set.getClassListIndex())+ ".txt", creator.getTextFieldContainer());
-                //creator.writeTextToFile("/Users/evy/Documents/GradeTracker-new/src/main/view/UserInfo/StudentInfo/" + set.getUsername() +"/"+classList.get(0)+ ".txt", creator.getTextFieldContainer());
             }
         });
         
@@ -176,7 +140,6 @@ public class StudentStatCollect extends JFrame {
 
        newTypeButton.addActionListener(new ActionListener() {
         public void actionPerformed(ActionEvent e) {
-            //typeNumber++;
             newSet();
             System.out.println("ClassLabelPanel after adding: "+ allBoxesPanel.getComponentCount());
         }
@@ -205,9 +168,7 @@ public class StudentStatCollect extends JFrame {
 
    private void correctGradeFormatChecker(Pattern pattern) {
         int index = 4;
-        for (int i=0; i<=7; i++) {
-            //component componentMatched = classLabelPanel.getComponent(0);
-
+        for (int i=1; i<=8; i++) { //max num of grade type
             //if it exists. otherwise, bye bye.
             String text = goIntoPanel(classLabelPanel, index); //will return a text box. probably just first for now??? //base case 5
             if (text.equals("does not exist")) {
@@ -215,35 +176,12 @@ public class StudentStatCollect extends JFrame {
                 break;
             }
             index = index + 3;
-            //JTextField textFieldOfComponent = (JTextField) componentMatched;
-            //String textOfComponent = textFieldOfComponent.getText();
             System.out.println("text before matcher works?"+text);
             Matcher matcher = pattern.matcher(text);
             Boolean matcherBoolean = matcher.find();
 
             System.out.println("matcher works?: "+ matcherBoolean);
         }
-
-
-        //barebones
-        // String filePath = "path/to/your/file.txt"; // Specify the path to your file
-
-        // try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
-        //     String line;
-        //     while ((line = reader.readLine()) != null) {
-        //         // Process each line of the file
-        //         System.out.println(line); // Print the line to the console
-        //     }
-        // } catch (IOException e) {
-        //     e.printStackTrace(); // Handle any IOException
-        // }
-        //
-        //index = 3
-        //for i in the file
-        //start at index 3
-        //check against regex
-        //if good, at 3
-        //if wrong send pop up
     }
 
     private String goIntoPanel(JPanel panel, int index) {
@@ -261,23 +199,16 @@ public class StudentStatCollect extends JFrame {
         if (component instanceof JTextField) {
                 JTextField textField = (JTextField) component;
                 String text = textField.getText();
-                System.out.println("about to return the text: "+ text);
                 return text;
             } 
 
         else if (component instanceof JPanel) {
-                //System.out.println();
                 JPanel jpanel = (JPanel) component;
-                //JPanel backToPanel = (JPanel) component;
-
                 String text = goIntoPanel(jpanel, 0);
 
                 if (text != null) {
                     return text;
                 }
-
-                //return "something went wrong StudentStatCollect";
-                //goIntoPanel(jpanel);
             }
 
             System.out.println("none of these" +component.getClass().getName());
@@ -285,21 +216,11 @@ public class StudentStatCollect extends JFrame {
 
     }
 
-    // For i in classes, give classes : class1 
-    //     for j in types :
-    //         type1gradeBox.
-
-
     //read classes array, first five classes
     private void DisplayClasses() {
-        ///
-        //creditTypeBox();
-        ///
         String filePath = "/Users/evy/Documents/GradeTracker-new/src/main/view/UserInfo/StudentInfo/" + set.getUsername() +"/class"+ ".txt";
         ArrayList<String> classList = set.getClassList();
-        //will be updated later
 
-        //for (String className : classList) {
         readClass(classList);
     }
 
@@ -309,16 +230,9 @@ public class StudentStatCollect extends JFrame {
         creditTypeBox();
         newSet();
 
-        //typeNumber++;
         container.add(classLabelPanel);
-        //set.setTextFieldPanel(classLabelPanel);
-        //set.setTextFieldContainer(container);
-
-        System.out.println("classLabelPanelcomps"+classLabelPanel.getComponentCount());
-        System.out.println("heloooooo"+ allBoxesPanel.getComponentCount());
         
         window.add(classLabelPanel, BorderLayout.CENTER);
-        //window.add(allBoxesPanel, BorderLayout.CENTER);
         creator.windowFix(window);
     }
 
@@ -327,15 +241,12 @@ public class StudentStatCollect extends JFrame {
             creator.hideContainer();
             JPanel northClassPanelContainer = new JPanel(new BorderLayout());
             JPanel classLabelPanelContainer = new JPanel(new BorderLayout());
-            //JLabel classLabel = new JLabel("h");
-            JLabel classLabel = new JLabel(set.getFinalClassList().get(set.getClassListIndex())); //new JLabel("h"); 
+            JLabel classLabel = new JLabel(set.getFinalClassList().get(set.getClassListIndex()));
             classLabelPanelContainer.add(classLabel);
             classLabelPanelContainer.setPreferredSize(new Dimension(155,50));
             northClassPanelContainer.add(classLabelPanelContainer, BorderLayout.NORTH);
             classLabelPanel.add(northClassPanelContainer);
-            //
             northClassPanelContainer.setBackground(Color.PINK);
-            //
             classLabel.setVisible(true);
             creator.windowFix(window);
             numOfBoxes++;
@@ -348,18 +259,10 @@ public class StudentStatCollect extends JFrame {
             JPanel gradePercentagePanel = new JPanel(new BorderLayout());
             JTextField gradePercentage = creator.createTextBox(window, "Percentage of Grade", 80, 10); //the sol
 
-            //allBoxesPanel.add(gradePercentage);
-            System.out.println("Components after adding: "+ allBoxesPanel.getComponentCount());
-
-            gradePercentagePanel.add(gradePercentage); //issue
+            gradePercentagePanel.add(gradePercentage);
             gradePercentagePanel.setPreferredSize(new Dimension(155,50));
             northGradePercentagePanel.add(gradePercentagePanel, BorderLayout.NORTH);
             classLabelPanel.add(northGradePercentagePanel);
-
-
-            ////
-            //gradePercentage.setPreferredSize(new Dimension(155,50));
-            ////
             creator.windowFix(window); 
             numOfBoxes++;
         }
