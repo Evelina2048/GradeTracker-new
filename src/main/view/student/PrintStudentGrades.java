43d06ba2f64f840acf0e7aa47f1c3f7e4b249db1
Evelina2048 <evelinaguenther@gmail.com>
2024-10-31 01:23:08 -0400

    print grades clean up

--- src/main/view/student/PrintStudentGrades.java
+++ src/main/view/student/PrintStudentGrades.java
@@ -17,28 +17,19 @@ import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.IntStream;
 import java.awt.event.ActionEvent;
-import controller.Creator;
-import controller.Decorator;
+
 import controller.FileHandling;
 import model.Set;
 import model.SetList;
-import model.SetState;
 import model.SetUserInformation;
 import controller.CompositeActionListenerWithPriorities;
 import controller.CreateButton;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.FlowLayout;
-import java.awt.LayoutManager;
 
 import javax.swing.AbstractAction;
 import javax.swing.BorderFactory;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
 import javax.swing.border.Border;
 
 public class PrintStudentGrades extends JFrame {
@@ -78,7 +69,6 @@ public class PrintStudentGrades extends JFrame {
         window.getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0), "enterAction");
         window.getRootPane().getActionMap().put("enterAction", enterAction);
         actionPriorities.setCurrentClass("PrintStudentGrades");
-
     }
 
     public void printStudentGradesLaunch() {
@@ -94,16 +84,11 @@ public class PrintStudentGrades extends JFrame {
 
         gradeList = new ArrayList<>();
         amountOfFinalGrade = new ArrayList<>();
-        //gradeTypeList = new ArrayList<>(); //10/2
         listOfAverages = new ArrayList<>();
         percentageOfGradeList = new ArrayList<>();
-        //10/18 for (int classIndex = 0; classIndex < setList.getFinalClassList().size(); classIndex++) { //go through classes
-            System.out.println("printgradestest "+setList.getFinalClassList().get(whichCurrClassIndex)); //classIndex
             String filePathForClass = setUserInformation.getPathToClassInformationFileWithChosenIndex(whichCurrClassIndex); //classIndex
-            System.out.println("made it past first");
             allTextListForClass = fileHandler.readFileToList(filePathForClass);
 
-            //grades starts at index 4
             printArray(allTextListForClass);
 
             ArrayList<String> tempList = new ArrayList<String>(Arrays.asList(allTextListForClass.get(3).split(" ")));
@@ -112,25 +97,14 @@ public class PrintStudentGrades extends JFrame {
             makePercentageOfGradeList();
 
             makeGradeTypeList();
-            System.out.println("hello print grades. grade type number is "+gradeTypeList.size()+" and the classes place is "+SetState.getInstance().getClassListIndex()+"/"+SetList.getInstance().getFinalClassList().size());
 
             makeListOfAveragesList();
 
             makeAmountOfFinalGradeList();
 
-        //10/18 }
-
         float total = getGradeTotal();
 
         addEverythingToWindow(total);
-        //window.add(allContainer);
-
-        // Component[] windowComponents3 = window.getContentPane().getComponents();
-        // int i = 0;
-        // for (Component windowComp : windowComponents3) {
-        //     System.out.println("compcount " + i + " classname "+ windowComp.getClass().getName() + " regname "+windowComp.getName() + " isvisible "+windowComp.isVisible());
-        //     i++;
-        // }
         actionPriorities.setCurrentClass("PrintStudentGrades");
     }
 
@@ -139,17 +113,14 @@ public class PrintStudentGrades extends JFrame {
         ArrayList <Integer> gradeTypeNumberList = setList.getGradeTypeList();
 
         int gradeTypeAmount = gradeTypeNumberList.get(whichCurrClassIndex);
-        System.out.println("gradeTypeAmount "+gradeTypeAmount);
         JPanel allContainer = new JPanel(new GridLayout((1+gradeTypeAmount),3,5,5)); //rows,cols //////
         allContainer.setName("allContainr");
         borderContainer = new JPanel(new BorderLayout());
 
-        JLabel placeholdType = new JLabel("Grade Type name: "+gradeTypeList.get(whichCurrClassIndex));//(gradeTypeIndex)); //10/25
-        System.out.println("placehold type for "+total+" name is "+placeholdType.getText()+" gradetypelist is "+gradeTypeList+" gradetypeindex "+gradeTypeIndex+" curr class index is "+whichCurrClassIndex);
+        JLabel placeholdType = new JLabel("Grade Type name: "+gradeTypeList.get(whichCurrClassIndex));
         placeholdType.setBorder(blackline);
-        placeholdType.setHorizontalAlignment(JLabel.CENTER); //TODO center might cause bugs 10/12
+        placeholdType.setHorizontalAlignment(JLabel.CENTER);
         borderContainer.add(placeholdType,BorderLayout.NORTH);
-
 
         borderContainer.add(allContainer,BorderLayout.CENTER);
 
@@ -157,8 +128,6 @@ public class PrintStudentGrades extends JFrame {
         finalgrade.setBorder(blackline);
         finalgrade.setHorizontalAlignment(JLabel.CENTER);
         borderContainer.add(finalgrade,BorderLayout.SOUTH);
-
-        Creator create = new Creator();
 
         JLabel average = new JLabel("Average");
         average.setBorder(blackline);
@@ -175,19 +144,7 @@ public class PrintStudentGrades extends JFrame {
         contributiongrade.setHorizontalAlignment(JLabel.CENTER);
         allContainer.add(contributiongrade);
 
-        //if ()
-        System.out.println("*gradetypenumberlist* "+gradeTypeNumberList);
-        System.out.println("&whichCurrClassIndex& "+whichCurrClassIndex);
         for (int i = 0; i < gradeTypeNumberList.get(whichCurrClassIndex); i++) { //shouldnt be for classes, should be for gradetypes then classes
-        //for (int i = 0; i < gradeTypeNumberList.get(gradeTypeIndex); i++) { //10/12
-            System.out.println("i "+gradeTypeNumberList);
-            System.out.println("i listOfAverages"+listOfAverages);
-            System.out.println("i 180 "+i);
-            System.out.println("listofaverages.get(i) "+ listOfAverages.get(i));
-            System.out.println("i "+listOfAverages.get(i));
-
-            System.out.println("listofavs "+listOfAverages);
-
             JLabel listOfAveragesLabel = new JLabel(String.valueOf(listOfAverages.get(i)));
             listOfAveragesLabel.setBorder(blackline);
             allContainer.add(listOfAveragesLabel);
@@ -196,7 +153,6 @@ public class PrintStudentGrades extends JFrame {
             percentageOfGradeLabel.setBorder(blackline);
             allContainer.add(percentageOfGradeLabel);
 
-            //percentageOfGradeLabel.setBorder(blackline); //10/25
             allContainer.add(new JLabel("heyo "+(String.valueOf((float) percentageOfGradeList.get(i)/100*gradeBoxTotal.get(i)))));
         }
         window.add(borderContainer);
@@ -204,65 +160,38 @@ public class PrintStudentGrades extends JFrame {
 
     private float getGradeTotal() {
         float total = 0;
-        //for (int i = 0; i<amountOfFinalGrade.size(); i++) {
-            // 10/19 total += amountOfFinalGrade.get(0);
             total += amountOfFinalGrade.get(gradeTypeIndex);
-        //}
         return total;
     }
 
     private void makeAmountOfFinalGradeList() {
-        //for (int boxSetIndex = 0; boxSetIndex < gradeList.size(); boxSetIndex++) { //8/24
         int averageXpercentageOfGrade;
         for (int boxSetIndex = 0; boxSetIndex < listOfAverages.size(); boxSetIndex++) {
-            System.out.println("listofaverages "+listOfAverages);
-            System.out.println("boxsetindex "+boxSetIndex);
-            System.out.println("listofaverages.getboxsetindex "+(listOfAverages.get(boxSetIndex)));
-            System.out.println("percentageOfGradeList "+(percentageOfGradeList));
-            System.out.println("listofaverages4 "+(percentageOfGradeList.get(boxSetIndex)));
-            System.out.println("8888 instead "+ "boxsetindex "+boxSetIndex+" list of averages "+listOfAverages+" percentageOfGradeList "+percentageOfGradeList);
             averageXpercentageOfGrade = listOfAverages.get(boxSetIndex)*percentageOfGradeList.get(boxSetIndex);
-            amountOfFinalGrade.add(((float) averageXpercentageOfGrade)/100); //LOOKIE
+            amountOfFinalGrade.add(((float) averageXpercentageOfGrade)/100);
         }
     }
 
     private void makeListOfAveragesList() {
         listOfAverages = new ArrayList<>();
-        //gradeBoxTotal = 0; //commented out 10/15 12:52AM
         ArrayList<ArrayList<String>> seperatedBySpaceListOfGrades = new ArrayList<ArrayList<String>>();
         System.out.println("listofaverages -3 "+allTextListForClass);
         for (int boxPanelIndex = 3; boxPanelIndex < allTextListForClass.size(); boxPanelIndex += 3) { //for each grades box
-            System.out.println("10/12/12:01 should print twice");
             List <String> gradeArrayForOneBox = Arrays.asList(allTextListForClass.get(boxPanelIndex).split(" "));
-            System.out.println("tempArray "+gradeArrayForOneBox);
 
             seperatedBySpaceListOfGrades.add(new ArrayList<>(gradeArrayForOneBox));
-            System.out.println("shouldbefinalseperatedbyspace "+seperatedBySpaceListOfGrades);
-            //10/13 seperatedBySpaceListOfGrades = new ArrayList<>(Arrays.asList(allTextListForClass.get(boxPanelIndex).split(" ")));
 
-            //10/13 12:47 gradeList.add(seperatedBySpaceListOfGrades);
-
-            System.out.println(("seperatedbyspacelistofgrades "+seperatedBySpaceListOfGrades));
-            System.out.println(("listofaverages -2.2 "+seperatedBySpaceListOfGrades.get(0)));
-            //for (ArrayList<String> grades : seperatedBySpaceListOfGrades) {
             int average = 0;
             for (int i = 0; i< seperatedBySpaceListOfGrades.size(); i++) {
-                //gradeList.add(IntStream.of(seperatedBySpaceListOfGrades.get(k)).sum());
                 int thisGradeBoxTotal = seperatedBySpaceListOfGrades.get(i).stream()
                             .mapToInt(Integer::parseInt)  // Convert String to int
                             .sum();
-                //10/13 12:47 gradeBoxTotal += Integer.parseInt(seperatedBySpaceListOfGrades.get(k));
                 gradeBoxTotal.add(thisGradeBoxTotal);
-                System.out.println("10/13 12:59 "+gradeBoxTotal);
 
                 average = gradeBoxTotal.get(i)/seperatedBySpaceListOfGrades.size();
             }
 
-            System.out.println("screen "+seperatedBySpaceListOfGrades + "size "+seperatedBySpaceListOfGrades.size());
-            System.out.println("listofaverages -2 "+average);
             listOfAverages.add(average);
-            System.out.println("listofaverages -1 "+listOfAverages);
-
         }
     }
 
@@ -299,42 +228,13 @@ public class PrintStudentGrades extends JFrame {
                         doBackButtonProcedure();
             }
                 }, 1, "backButton", null, "PrintStudentGrades");
-                System.out.println("in  printstudentgrades currclass "+actionPriorities.getCurrentClass());
     }});
 
-
-        //\\
-        // backButton.addActionListener(new ActionListener() {
-        //     public void actionPerformed(ActionEvent e) {
-        //         actionPriorities.setCurrentClass(currentClass);
-        //         actionPriorities.addClassActionListener(new ActionListener() {
-        //             @Override
-        //             public void actionPerformed(ActionEvent e) {// remember wont run  if just enter without a click
-        //                 saveButtonAction();
-        //                 hideWindow();
-
-        //                 set.setWindow(window);
-        //                 MainWindow main = new MainWindow();
-        //                 main.MainWindowLaunch();
-        //                 main.setButtonSelected();
-
-        //                 window.removeWindowListener(windowCloseAdapter);
-        //             }
-        //     }, 1, "backButton", null, currentClass);  // Add this ActionListener with priority 1
-        // }});
-        //\\
-
         JButton nextButton = createButton.nextButtonCreate();
-        
-        //nextButton.setText("");
 
         JPanel nextButtonPanel = new JPanel();
         nextButtonPanel.add(nextButton);
-        // System.out.println("hello print grades. grade type number is "+gradeTypeList.size()+" and the classes place is "+SetState.getInstance().getClassListIndex()+"/"+SetList.getInstance().getFinalClassList().size());
-        //10/21 Boolean atEndOfClasses = SetState.getInstance().getClassListIndex() == SetList.getInstance().getFinalClassList().size();
         Boolean atEndOfClasses = whichCurrClassIndex == SetList.getInstance().getFinalClassList().size() -1 ; //-1 to account for index starting from 0 and size not
-        //0 ==
-        //at first grade with two classes with one grade each
         if (atEndOfClasses) {
             nextButton.setEnabled(false);
 
@@ -357,7 +257,7 @@ public class PrintStudentGrades extends JFrame {
             hideWindow();
             whichCurrClassIndex--; //only if there are more
             printStudentGradesLaunch();
-            actionPriorities.setCurrentClass("PrintStudentGrades"); //1
+            actionPriorities.setCurrentClass("PrintStudentGrades");
         }
 
         else {
@@ -386,8 +286,8 @@ public class PrintStudentGrades extends JFrame {
         System.out.println("confirmation");
         backNextButtonsPanel.setVisible(false);
         borderContainer.setVisible(false);
-        borderContainer.removeAll(); //10/21
-        backNextButtonsPanel.removeAll(); //10/21
+        borderContainer.removeAll();
+        backNextButtonsPanel.removeAll();
 
         Component[] windowComponents3 = window.getContentPane().getComponents();
         int i = 0;
